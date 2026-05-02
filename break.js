b80876a339cd1bb1dec15d05e3f33a0a909b5582
Alok Rajbongshi <alokraj00061@gmail.com>
2026-05-02 19:27:15 +0600

    continue keyword added

--- break.js
+++ break.js
@@ -1,6 +1,14 @@
 for (let i = 1; i < 20; i++) {
-    console.log(i);
+    // console.log(i);
     if (i >= 15) {
         break
     }
+}
+
+// continue
+for (let i = 1; i < 20; i++) {
+    if (i % 2 == 1) {
+        continue;
+    }
+    console.log(i);
 }
